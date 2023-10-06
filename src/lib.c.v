ec384529ffee23a012622ede25fa7d7e647059e4
CrocAI <monytiktok001@gmail.com>
2023-10-06 03:13:31 +0200

    fix: compiling with recent upstream changes

--- src/lib.c.v
+++ src/lib.c.v
@@ -19,8 +19,6 @@ pub:
 	window       Window    // The window object number
 	event_type   EventType // Event type
 	element      &char     // HTML element ID
-	data         &char     // JavaScript data
-	size         usize     // JavaScript data len
 	event_number usize     // Internal Webinix
 }
 
@@ -66,7 +64,7 @@ fn C.webinix_return_string(e &C.webinix_event_t, s &char)
 fn C.webinix_return_bool(e &C.webinix_event_t, b bool)
 
 // -- Interface ----------------------- // not wrapped
-fn C.webinix_interface_bind(win Window, element &char, func fn (win Window, event_type EventType, element &char, data &char, data_size usize, event_num usize)) Function
+fn C.webinix_interface_bind(win Window, element &char, func fn (win Window, event_type EventType, element &char, event_num usize)) Function
 fn C.webinix_interface_set_response(win Window, event_num usize, resp &char)
 fn C.webinix_interface_is_app_running() bool
 fn C.webinix_interface_get_window_id(win Window) Window
